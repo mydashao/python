176c38a3b98e69afe0023471b1699e276334b70e
mydashao <dashao.yu@qq.com>
2019-03-28 14:30:30 +0800

    from home

--- com/myfish/download_pic.py
+++
@@ -1,8 +0,0 @@
-detail_url = "https://burst.shopify.com/photos/braving-the-cold-weather-running-in-ottawa"
-from  selenium import webdriver
-
-option = webdriver.Chrome()
-option.add_argument('--headless')
-browser = webdriver.Chrome(chrome_options=option)
-browser.get(detail_url)
-print(browser.page_source)
